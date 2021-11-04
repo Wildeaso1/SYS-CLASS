f50b878764b80ebd3bcb56833f29bb2026fde5af
Wildeaso1 <70636740+Wildeaso1@users.noreply.github.com>
2021-11-04 20:47:52 +0100

    Mm class

---
+++ C-SYSLes4/SpriteInheritance/SpriteInheritance/Pingu.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace Eindopdracht
+{
+
+    class Pingu : Sprite
+    {
+        public Pingu(RectangleF screenPosition)
+        {
+            this.screenPosition = screenPosition;
+            imageFrame = new Rectangle(95, 457, 16, 16);
+        }
+    }
+}
---
+++ C-SYSLes5/Knight/Knight/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Knight
+{
+    abstract class Equipment
+    {
+        public int x;
+        public int y;
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public virtual void Equip()
+        {
+            Console.WriteLine("Equipment slot");
+        }
+
+        public virtual void Hitbymouse(int x, int y)
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+
+    abstract class Consumable
+    {
+        public virtual void Consume()
+        {
+            Console.WriteLine("Slurp");
+        }
+
+        public virtual void Spend()
+        {
+            Console.WriteLine("Mmmm.. Money");
+        }
+
+        public bool CanConsume = true;
+    }
+
+    class Armor : Equipment
+    {
+        public override void Equip()
+        {
+            Console.WriteLine("Armor slot");
+        }
+
+        public override void Hitbymouse(int x, int y)
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+
+    class Weapon : Equipment
+    {
+        public override void Equip()
+        {
+            Console.WriteLine("Weapon slot");
+        }
+
+        public override void Hitbymouse(int x, int y)
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+
+    class Money : Consumable
+    {
+        public bool CanPickup = true;
+    }
+
+    class PotionHeal : Consumable
+    {
+        public override void Consume()
+        {
+            Console.WriteLine("Mm.. Good soup");
+        }
+    }
+
+    class PotionMana : Consumable
+    {
+        public override void Consume()
+        {
+            Console.WriteLine("Mm.. Mana");
+        }
+    }
+
+    
+}
